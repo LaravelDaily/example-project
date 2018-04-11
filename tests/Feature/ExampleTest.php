507e05666988421e126842df1860e4ebdcbe2780
Karolis <narkevicius.karolis@gmail.com>
2018-04-11 14:21:55 +0300

    phpunit empty assertions

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,6 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        $this->assertTrue(true);
 //        $response = $this->get('/');
 
 //        $response->assertStatus(200);
